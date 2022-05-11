1a21a92d0dfd69f8a61f13cba54a2930f31a2109
Luca Pacher <pacher@to.infn.it>
2022-05-11 06:35:19 +0200

    ENH: 4-bit ripple-counter using structural code

---
+++ fpga/labs/lab6/.solutions/bench/tb_RippleCounter4b.v
@@ -0,0 +1,55 @@
+//
+// Testbench module for 4-bit ripple-counter.
+//
+// Luca Pacher - pacher@to.infn.it
+// Spring 2022
+//
+
+`timescale 1ns / 100ps
+
+module tb_RippleCounter4b ;
+
+
+   /////////////////////////
+   //   clock generator   //
+   /////////////////////////
+
+   wire clk, clk_buf ;
+
+   ClockGen  #(.PERIOD(100.0)) ClockGen_inst (.clk(clk) ) ;   // override default period as module parameter (default is 50.0 ns)
+
+
+   ////////////////////////////////////////////////
+   //   example Xilinx primitive instantiation   //
+   ////////////////////////////////////////////////
+
+   IBUF IBUF_inst ( .I(clk), .O(clk_buf) ) ;
+
+
+   ///////////////////////////
+   //   device under test   //
+   ///////////////////////////
+
+   reg rst = 1'b0 ;
+
+   wire [3:0] count ;
+
+   RippleCounter4b  DUT (.clk(clk_buf), .rst(rst), .Q(count) ) ;
+
+
+   //////////////////
+   //   stimulus   //
+   //////////////////
+
+   initial begin
+
+      #100 rst = 1'b1 ;              // assert the reset
+      #150 rst = 1'b0 ;              // release the reset
+
+      #(20*100 + 17) rst = 1'b1 ;    // explore all possible output codes, then assert an asynchronous reset and check what happens
+
+      #500 $finish ;
+   end
+
+endmodule
+
---
+++ fpga/labs/lab6/.solutions/rtl/RippleCounter4b.v
@@ -0,0 +1,62 @@
+//
+// Example Verilog implementation for a simple 4-bit asynchronous ripple counter using structural code.
+//
+// Luca Pacher - pacher@to.infn.it
+// Spring 2022
+//
+
+
+`timescale 1ns / 100ps
+
+module DFF (
+
+   input  wire clk,   // clock
+   input  wire rst,   // reset, active-high (then can be synchronous or asynchronous according to sensitivity list)
+   input  wire D,
+   output reg Q, Qbar
+
+   ) ;
+
+   // **NOTE: can be replaced by 'always_ff' in SystemVerilog
+
+   //always @(posedge clk) begin                       // synchronous reset
+   always @(posedge clk or posedge rst) begin      // asynchronous reset
+
+      if (rst) begin      // same as if (rst == 1'b1)
+         Q <= 1'b0 ;      // **NOTE: inside clocked always blocks use a NON-BLOCKING assignment <= instead of a BLOCKING assignment =
+      end
+      else begin
+         Q <= D ;
+      end
+   end  // always
+
+
+   // inverted output
+   assign Qbar = ~ Q ;
+
+endmodule
+
+//_______________________________________________________________
+
+
+`timescale 1ns / 100ps
+
+module RippleCounter4b (
+
+   input  wire clk, rst,
+   output wire [3:0] Q
+
+   ) ;
+
+   // 4-bit bus for internal wiring
+   wire [3:0] Qbar ;
+
+
+   // structural code
+   DFF  ff_0 ( .rst( rst ), .clk( clk     ), .D( Qbar[0] ), .Q( Q[0] ), .Qbar( Qbar[0]) ) ;
+   DFF  ff_1 ( .rst( rst ), .clk( Qbar[0] ), .D( Qbar[1] ), .Q( Q[1] ), .Qbar( Qbar[1]) ) ;
+   DFF  ff_2 ( .rst( rst ), .clk( Qbar[1] ), .D( Qbar[2] ), .Q( Q[2] ), .Qbar( Qbar[2]) ) ;
+   DFF  ff_3 ( .rst( rst ), .clk( Qbar[2] ), .D( Qbar[3] ), .Q( Q[3] ), .Qbar( Qbar[3]) ) ;
+
+endmodule
+
